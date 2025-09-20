5339a853b254ac4ae88d0804bd96852b9bbfe087
neoarz <tyrantneo740@gmail.com>
2025-09-20 09:32:21 -0400

    feat(labubu): make labubu command and give bot more permissions

--- bot.py
+++ bot.py
@@ -17,18 +17,7 @@ from database import DatabaseManager
 load_dotenv()
 
 """	
-Setup bot intents (events restrictions)
-For more information about intents, please go to the following websites:
-https://discordpy.readthedocs.io/en/latest/intents.html
-https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents
-
-
 Default Intents:
-intents.bans = True
-intents.dm_messages = True
-intents.dm_reactions = True
-intents.dm_typing = True
-intents.emojis = True
 intents.emojis_and_stickers = True
 intents.guild_messages = True
 intents.guild_reactions = True
@@ -37,27 +26,24 @@ intents.guild_typing = True
 intents.guilds = True
 intents.integrations = True
 intents.invites = True
-intents.messages = True # `message_content` is required to get the content of the messages
 intents.reactions = True
 intents.typing = True
 intents.voice_states = True
 intents.webhooks = True
 
-Privileged Intents (Needs to be enabled on developer portal of Discord), please use them only if you need them:
 intents.members = True
-intents.message_content = True
 intents.presences = True
 """
 
 intents = discord.Intents.default()
-
-"""
-Uncomment this if you want to use prefix (normal) commands.
-It is recommended to use slash commands and therefore not use prefix commands.
-
-If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
-"""
 intents.message_content = True
+intents.bans = True
+intents.dm_messages = True
+intents.dm_reactions = True
+intents.dm_typing = True
+intents.emojis = True
+intents.messages = True 
+
 
 
 
