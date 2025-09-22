e0147a683d812ff30e1edbf145079375ff37617e
neoarz <tyrantneo740@gmail.com>
2025-09-22 08:42:38 -0400

    fix(invite): remove permission addons
    you can add permissions from the discord dev portal itself

--- cogs/owner/invite.py
+++ cogs/owner/invite.py
@@ -33,12 +33,8 @@ class Invite(commands.Cog, name="invite"):
         if client is None:
             await context.send("Bot is not ready. Try again shortly.")
             return
-        permissions = os.getenv("INVITE_PERMISSIONS", "0")
-        invite_url = (
-            f"https://discord.com/api/oauth2/authorize?client_id={client.id}"
-            f"&scope=bot%20applications.commands&permissions={permissions}"
-        )
-        embed = discord.Embed(title="Invite", description=f"Invite me by clicking [here]({invite_url}).", color=0x7289DA)
+        invite_link = os.getenv("INVITE_LINK")
+        embed = discord.Embed(title="Install", description=f"Install me by clicking [here]({invite_link}).", color=0x7289DA)
         embed.set_author(name="Owner", icon_url="https://yes.nighty.works/raw/zReOib.webp")
         
         try:
