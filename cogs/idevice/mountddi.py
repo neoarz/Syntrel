8d5db7f4cc38480c18fb7e2d377a9e45a53f1c2a
neoarz <tyrantneo740@gmail.com>
2025-09-26 21:05:13 -0400

    fix(mountddi): fix so that it doesnt timeout

--- cogs/idevice/mountddi.py
+++ cogs/idevice/mountddi.py
@@ -1,8 +1,6 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
-import time
 import os
 
 
@@ -11,27 +9,33 @@ class Mountddi(commands.Cog, name="mountddi"):
         self.bot = bot
 
     @commands.hybrid_command(
-        name="mountddi", description="How to manually mount DDI"
+        name="mountddi",
+        description="How to manually mount DDI"
     )
-    async def mountddi(self, context: Context) -> None:
+    async def mountddi(self, ctx: Context) -> None:
+        await ctx.defer()
+
         embed = discord.Embed(
             color=0xfa8c4a,
             description=(
-                '# How to Manually Mount DDI\n\n---\n\n' +
-                '1. **Download the DDI.zip file attached above:**\n' +
-                '   - Save it to your device and extract the contents\n\n' +
-                '2. **Replace the DDI folder in StikDebug:**\n' +
-                '   - Navigate to the StikDebug default directory on your iPhone/iPad\n' +
-                '   - Delete the existing DDI folder completely\n' +
-                '   - Replace it with the DDI folder from the downloaded zip\n' +
-                '   - Make sure it\'s in the StikDebug default directory\n\n' +
-                '3. **Restart and retry:**\n' +
-                '   - Completely restart StikDebug\n' +
+                '# How to Manually Mount DDI\n\n---\n\n'
+                '1. **Download the DDI.zip file attached above:**\n'
+                '   - Save it to your device and extract the contents\n\n'
+                '2. **Replace the DDI folder in StikDebug:**\n'
+                '   - Navigate to the StikDebug default directory on your iPhone/iPad\n'
+                '   - Delete the existing DDI folder completely\n'
+                '   - Replace it with the DDI folder from the downloaded zip\n'
+                '   - Make sure it\'s in the StikDebug default directory\n\n'
+                '3. **Restart and retry:**\n'
+                '   - Completely restart StikDebug\n'
                 '   - See if you get the same error again\n\n'
             )
         )
-        embed.set_author(name="idevice", icon_url="https://yes.nighty.works/raw/snLMuO.png")
-        embed.set_footer(text=f'Last Edited by neoarz')
+        embed.set_author(
+            name="idevice",
+            icon_url="https://yes.nighty.works/raw/snLMuO.png"
+        )
+        embed.set_footer(text="Last Edited by neoarz")
         embed.timestamp = discord.utils.utcnow()
 
         view = discord.ui.View()
@@ -42,26 +46,14 @@ class Mountddi(commands.Cog, name="mountddi"):
             emoji="<:githubicon:1417717356846776340>"
         ))
 
+
         ddi_file_path = os.path.join(os.path.dirname(__file__), 'files/DDI.zip')
-        
-        try:
-            if context.interaction:
-                if os.path.exists(ddi_file_path):
-                    file = discord.File(ddi_file_path, filename='DDI.zip')
-                    await context.interaction.response.send_message(embed=embed, view=view, file=file)
-                else:
-                    await context.interaction.response.send_message(embed=embed, view=view)
-            else:
-                if os.path.exists(ddi_file_path):
-                    file = discord.File(ddi_file_path, filename='DDI.zip')
-                    await context.send(embed=embed, view=view, file=file)
-                else:
-                    await context.send(embed=embed, view=view)
-        except discord.NotFound:
-            if context.interaction:
-                await context.interaction.followup.send(embed=embed, view=view)
-            else:
-                await context.send(embed=embed, view=view)
+        file = discord.File(ddi_file_path, filename='DDI.zip') if os.path.exists(ddi_file_path) else None
+
+        if file:
+            await ctx.send(embed=embed, view=view, file=file)
+        else:
+            await ctx.send(embed=embed, view=view)
 
 
 async def setup(bot) -> None:
