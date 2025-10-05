6fc24ef8f641be8dcf173b5edccdfbe0619cb0d5
neoarz <tyrantneo740@gmail.com>
2025-10-04 20:21:10 -0400

    fix(feedback): wasnt working due to app commands

--- cogs/general/feedback.py
+++ cogs/general/feedback.py
@@ -17,31 +17,36 @@ class FeedbackForm(discord.ui.Modal, title="Feeedback"):
         self.stop()
 
 def feedback_command():
-    @app_commands.command(
-        name="feedback", description="Submit a feedback for the owners of the bot"
+    @commands.hybrid_command(
+        name="feedback",
+        description="Submit a feedback for the owners of the bot"
     )
-    async def feedback(self, interaction: discord.Interaction):
-        feedback_form = FeedbackForm()
-        await interaction.response.send_modal(feedback_form)
+    async def feedback(self, context):
+        if getattr(context, "interaction", None):
+            interaction = context.interaction
+            feedback_form = FeedbackForm()
+            await interaction.response.send_modal(feedback_form)
 
-        await feedback_form.wait()
-        interaction = feedback_form.interaction
-        await interaction.response.send_message(
-            embed=discord.Embed(
-                title="Thank You!",
-                description="Your feedback has been submitted, the owners have been notified about it.",
-                color=0x7289DA,
-            ).set_author(name="Feedback System", icon_url="https://yes.nighty.works/raw/gSxqzV.png"),
-            ephemeral=True,
-        )
+            await feedback_form.wait()
+            interaction = feedback_form.interaction
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Thank You!",
+                    description="Your feedback has been submitted, the owners have been notified about it.",
+                    color=0x7289DA,
+                ).set_author(name="Feedback System", icon_url="https://yes.nighty.works/raw/gSxqzV.png"),
+                ephemeral=True,
+            )
 
-        app_owner = (await self.bot.application_info()).owner
-        await app_owner.send(
-            embed=discord.Embed(
-                title="New Feedback",
-                description=f"{interaction.user} (<@{interaction.user.id}>) has submitted a new feedback:\n```\n{feedback_form.answer}\n```",
-                color=0x7289DA,
-            ).set_author(name="Feedback System", icon_url="https://yes.nighty.works/raw/gSxqzV.png")
-        )
+            app_owner = (await self.bot.application_info()).owner
+            await app_owner.send(
+                embed=discord.Embed(
+                    title="New Feedback",
+                    description=f"{interaction.user} (<@{interaction.user.id}>) has submitted a new feedback:\n```\n{feedback_form.answer}\n```",
+                    color=0x7289DA,
+                ).set_author(name="Feedback System", icon_url="https://yes.nighty.works/raw/gSxqzV.png")
+            )
+        else:
+            await context.send("This command can only be used as a slash command. Please use `/general feedback` instead.")
     
     return feedback
