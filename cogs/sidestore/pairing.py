1b636e6621d417b582ba2d8efdf5e11f71a031c6
CelloSerenity <195480169+CelloSerenity@users.noreply.github.com>
2025-10-05 21:35:43 -0600

    GONNA

--- cogs/sidestore/pairing.py
+++ cogs/sidestore/pairing.py
@@ -22,11 +22,11 @@ def pairing_command():
                 '   - Rename the file to `ALTPairingFile.mobiledevicepairing`\n' +
                 '   - Try moving the pairing file to the root directory of the SideStore folder in the Files app under "On My iPhone/iPad"\n\n' +
                 '3. **Certificate Signing:**\n' +
-                "   When signing Sidestore with certain certificates, you won't be able to select the pairing file from within the app\n" +
+                "   When signing SideStore with certain certificates, you won't be able to select the pairing file from within the app\n" +
                 '   - Try the fix mentioned above\n' +
-                '   - If you do not see the Sidestore folder in the Files app:\n' +
+                '   - If you do not see the SideStore folder in the Files app:\n' +
                 '     • Connect your phone to your computer\n' +
-                '     • Drag and drop the pairing file into the Sidestore app\'s files section\n' +
+                '     • Drag and drop the pairing file into the SideStore app\'s files section\n' +
                 '     • Ensure the file is renamed to `ALTPairingFile.mobiledevicepairing`\n'
             )
         )
