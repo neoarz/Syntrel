32b5f1cc32f47887616ba453becb24d1797f0ab0
CelloSerenity <195480169+CelloSerenity@users.noreply.github.com>
2025-10-05 21:42:02 -0600

    GONNA

--- cogs/sidestore/udid.py
+++ cogs/sidestore/udid.py
@@ -14,7 +14,7 @@ def udid_command():
             color=0x8e82f9,
             description=(
                 '# SideStore Could Not Determine Device UDID\n\n---\n\n' +
-                'This error usually occurs when the pairing file is corrupted. Please generate a new pairing file and try again.\n\n' +
+                'This error usually occurs when the pairing file is corrupted. Please create a new pairing file using `idevice_pair` and try again.\n\n' +
                 'If you forgot how to generate a new pairing file, you can refer to the documentation below.'
             )
         )
