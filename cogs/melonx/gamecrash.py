59792a6eee6a523f10c6a105a94ef070720ee5e5
Meshal Alharbi <108165195+Meshal-GIT@users.noreply.github.com>
2025-10-17 12:21:35 +0300

    add links to melonx discord server

--- cogs/melonx/gamecrash.py
+++ cogs/melonx/gamecrash.py
@@ -35,6 +35,12 @@ def crash_command():
             style=discord.ButtonStyle.secondary,
             url="https://github.com/neoarz/Syntrel/blob/main/cogs/melonx/gamecrash.py",
             emoji="<:githubicon:1417717356846776340>"
+        ))                
+        view.add_item(discord.ui.Button(
+            label="Join The MeloNX Discord Server", 
+            style=discord.ButtonStyle.primary, 
+            url="https://discord.gg/WEX6mUbq5c",
+            emoji="<:discord:1428671438071791736>>"
         ))
 
         if context.interaction:
