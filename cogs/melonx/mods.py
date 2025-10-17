dfc2843a35a0f7bb604cd57e45eb36ef36d49604
Meshal Alharbi <108165195+Meshal-GIT@users.noreply.github.com>
2025-10-17 12:23:53 +0300

    add link to melonx discord server

--- cogs/melonx/mods.py
+++ cogs/melonx/mods.py
@@ -37,7 +37,13 @@ def mods_command():
             url="https://github.com/neoarz/Syntrel/blob/main/cogs/melonx/mods.py",
             emoji="<:githubicon:1417717356846776340>"
         ))
-
+        view.add_item(discord.ui.Button(
+            label="Join The MeloNX Discord Server", 
+            style=discord.ButtonStyle.primary, 
+            url="https://discord.gg/WEX6mUbq5c",
+            emoji="<:discord:1428671438071791736>>"
+        ))
+        
         if context.interaction:
             await context.interaction.response.send_message(embed=embed, view=view)
         else:
