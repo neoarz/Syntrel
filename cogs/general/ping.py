1da81e68d69198f85644dde3bb977d3c84ca005f
neoarz <tyrantneo740@gmail.com>
2025-10-25 15:32:22 -0400

    chore: ping command not empheral

--- cogs/general/ping.py
+++ cogs/general/ping.py
@@ -16,9 +16,9 @@ def ping_command():
         if getattr(context, "interaction", None):
             inter = context.interaction
             if not inter.response.is_done():
-                await inter.response.send_message(embed=embed, ephemeral=True)
+                await inter.response.send_message(embed=embed, ephemeral=False)
             else:
-                await inter.followup.send(embed=embed, ephemeral=True)
+                await inter.followup.send(embed=embed, ephemeral=False)
         else:
             await context.send(embed=embed)
     
