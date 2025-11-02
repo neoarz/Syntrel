951009696beabf656d9f958254503e92f6a077a4
Meshal Alharbi <108165195+Meshal-GIT@users.noreply.github.com>
2025-11-02 17:46:00 +0300

    update /melonx 26
    stable released so changed it to make sure you are using the latest release of MeloNX

--- cogs/melonx/ios26.py
+++ cogs/melonx/ios26.py
@@ -18,8 +18,7 @@ def ios26_command():
                 '1. Make sure StikDebug is on the latest version.\n' +
                 '2. Turn on "Picture in Picture" in StikDebug\'s settings.\n\n' +
                 '### MeloNX\n' +
-                'Make sure you\'re on the latest public beta.\n' +
-                'https://discord.com/channels/1300369899704680479/1412931489817034892\n\n' +
+                'Make sure you\'re on the latest version of MeloNX.\n' +
                 '## Disclaimer:\n\n' +
                 'If you\'re on iOS 18 or below, and emulation is essential to you:\n\n' +
                 '## <:error:1424007141768822824> DO NOT UPDATE <:error:1424007141768822824> \n\n' +
