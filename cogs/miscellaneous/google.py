f0436fa8541ac526a269d40c3add4027331636e1
slinkybuky <105521195+slinkybuky@users.noreply.github.com>
2026-06-06 14:37:13 -0700

    google for dummies

---
+++ cogs/miscellaneous/google.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+
+def google_command():
+    @commands.hybrid_command(
+        name="google",
+        description="Search it using this handy dandy tool",
+    )
+    async def google(self, context):
+        message = "https://google.com"
+
+        if getattr(context, "interaction", None):
+            inter = context.interaction
+            if not inter.response.is_done():
+                await inter.response.send_message(message)
+            else:
+                await inter.followup.send(message)
+        else:
+            await context.send(message)
+
+    return google
